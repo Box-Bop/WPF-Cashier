99099cb173e6779660e6e216bd652221fcc9fd48
Box-Bop <Box-Bop@github.com>
2018-03-28 21:37:22 +0300

    All done, there seems to be no "hidden features"

--- WPF Shop/WPF Shop/MainWindow.xaml.cs
+++ WPF Shop/WPF Shop/MainWindow.xaml.cs
@@ -23,43 +23,85 @@ namespace WPF_Shop
         public MainWindow()
         {
             InitializeComponent();
+            AddButton.IsEnabled = false;
+            RemoveButton.IsEnabled = false;
         }
-        List<Product> product = new List<Product>();
+        List<Product> ProductList = new List<Product>();
+        List<Product> BasketList = new List<Product>();
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            product.Add(new Product() { ProductName = ProductNameBox.Text, Cost = double.Parse(ProductPriceBox.Text), Amount = int.Parse(ProductQuantityBox.Text) });
-            StockList.Items.Add(new Product() { ProductName = ProductNameBox.Text, Cost = double.Parse(ProductPriceBox.Text), Amount = int.Parse(ProductQuantityBox.Text) });
+            var identicalName = ProductList.FirstOrDefault(x => x.ProductName.Contains(ProductNameBox.Text));
+
+            if (Convert.ToDouble(ProductPriceBox.Text) > 0 && identicalName == null)
+            {
+                ProductList.Add(new Product { ProductName = ProductNameBox.Text, Cost = double.Parse(ProductPriceBox.Text) });
+                StockList.ItemsSource = null;
+                StockList.ItemsSource = ProductList;
+                ProductNameBox.Clear();
+                ProductPriceBox.Clear();
+                AddButton.IsEnabled = true;
+            }
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var item in StockList.SelectedItems)
+            if (StockList.SelectedIndex > -1)
             {
-                if (StockList.SelectedItems != null)
+                var match = BasketList.Where(x => String.Equals(x.ProductName, ProductList[StockList.SelectedIndex].ProductName, StringComparison.CurrentCulture));
+                if (match.Any())
                 {
-                    if (CustomerList.Items.Count == 0)
+                    foreach (var item in match)
                     {
-                        product.Where(x => x.ProductName == x.ProductName).ToList().ForEach(y => y.Amount = y.Amount - (y.Amount - 1));
-                        CustomerList.Items.Add(product);
-                    }
-                    else
-                    {
-                        //foreach (ListViewItem prB in CustomerList.Items)
-                        //{
-                        //product.Where(x => x.ProductName == x.ProductName
-                        if (!CustomerList.Items.Contains(product.Where(x => x.ProductName == x.ProductName)))
-                        {
-                            product.Where(x => x.ProductName == StockList.SelectedItem.ToString()).ToList().ForEach(y => y.Amount = y.Amount - (y.Amount - 1));
-                            CustomerList.Items.Add(product);
-                        }
-                        else
-                        {
-                            product.Where(x => x.ProductName == x.ProductName).ToList().ForEach(y => y.Amount = y.Amount + 1);
-                            CustomerList.Items.Add(product);
-                        }
-                        //}
+                        item.Amount += int.Parse(AddAmountBox.Text);
                     }
                 }
+                else
+                {
+                    BasketList.Add(ProductList[StockList.SelectedIndex]);
+
+                    BasketList[BasketList.Count - 1].Amount = int.Parse(AddAmountBox.Text);
+                }
+                CustomerList.ItemsSource = null;
+                CustomerList.ItemsSource = BasketList;
+                RemoveButton.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("You can't purchase air, please select a product.");
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            int index = CustomerList.SelectedIndex;
+            if (index > -1)
+            {
+                BasketList.RemoveAt(index);
+
+                CustomerList.ItemsSource = null;
+                CustomerList.ItemsSource = BasketList;
+            }
+            else
+            {
+                MessageBox.Show("Subtracting 1 air from 0 air would give you 2,147,483,646 air, please don't.");
+            }
+        }
+
+        private void PurchaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (BasketList.Count != 0)
+            {
+                double totalPrice = 0;
+                foreach (var item in BasketList)
+                {
+                    totalPrice += item.Amount * item.Cost;
+                }
+                MessageBox.Show("Your purchase was made, the total price is: " + Convert.ToString(totalPrice));
+            }
+            else
+            {
+                MessageBox.Show("Your purchase of \"air\" will cost you 0$");
             }
         }
     }
